00f5f3b333a851e3d97e28ad80461c369f65aeee
BrandMaverick <garg.sanjay@gmail.com>
2019-04-23 13:36:10 -0700

    dish components 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            dishes: DISHES
-        };
-    }
-
   render() {
       return (        
-          <div className="App">
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Name of Restaurant</NavbarBrand>
-                </div>
-              </Navbar>
-              <Menu dishes={this.state.dishes} />
+          <div>
+              <Main />
           </div>
     );
   }
