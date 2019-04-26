b8202029bd9f557ca2e7903ae6f6d7fae168eaf1
BrandMaverick <garg.sanjay@gmail.com>
2019-04-26 13:54:57 -0700

    redux - part 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends React.Component {
 
   render() {
       return (        
-          <HashRouter>
-            <div>
-              <Main />
-            </div>
-          </HashRouter>
+          <Provider store={store}>
+              <HashRouter>
+                <div>
+                  <Main />
+                </div>
+              </HashRouter>
+          </Provider>
     );
   }
 }
